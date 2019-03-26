5b88ab657e79c5b74b7fa0cefa5c528315335cd4
Mike Erickson <codedungeon@gmail.com>
2019-03-26 14:39:05 -0700

    chore: complete template integration, update configuration information

--- config/craftsman.php
+++ config/craftsman.php
@@ -3,9 +3,9 @@
 return [
 
     /*
-     |--------------------------------------------------------------------------
+     |-----------------------------------------------------------------------------
      | Craftsman Paths
-     |--------------------------------------------------------------------------
+     |------------------------------------------------------------------------------
      |
      | This value determines the "paths" which will be used when crafting assets
      | In most cases, these are the sensible defaults used by Laravel.
@@ -22,13 +22,13 @@ return [
     ],
 
     /*
-     |--------------------------------------------------------------------------
+     |-----------------------------------------------------------------------------
      | Templates
-     |--------------------------------------------------------------------------
+     |------------------------------------------------------------------------------
      |
      | This value determines the default template paths for each asset
      | If you wish to override any of these templates, you should create them
-     | in a separate directory (located in app directory) and update the path
+     | in using the following naming convention `templates/class.mustache.user.php`
      | accordingly. You should not modify the default templates directly as they
      | will be overwritten during any updates..
      */
--- tasks/build.sh
+++ tasks/build.sh
@@ -5,7 +5,8 @@ source ./tasks/messenger.sh
 rm -rf builds/
 php laravel-craftsman app:build
 rm -rf builds/templates
-cp -r templates builds/
+cp -r templates builds/templates
+cp config.php builds
 
 printf "\n"
 success "Build Completed Successfully" " SUCCESS "
