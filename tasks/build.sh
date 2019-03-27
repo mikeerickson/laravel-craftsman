9036f0a0c9c039f38ec1538d87136441e22b5f36
Mike Erickson <codedungeon@gmail.com>
2019-03-27 12:08:08 -0700

    chore: preparing for deployment

--- tasks/build.sh
+++ tasks/build.sh
@@ -2,7 +2,6 @@
 
 source ./tasks/messenger.sh
 
-rm -rf builds/
 php laravel-craftsman app:build
 rm -rf builds/templates
 cp -r templates builds/templates
@@ -10,3 +9,9 @@ cp config.php builds
 
 printf "\n"
 success "Build Completed Successfully" " SUCCESS "
+
+if [[ "$@" == "--deploy" ]]; then
+    ./tasks/deploy.sh
+fi
+
+
--- tasks/deploy.sh
+++ tasks/deploy.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+source ./tasks/messenger.sh
+
 APP_PATH="~/laravel-craftsman"
 
 if [[ -d "$APP_PATH" ]]; then
     mkdir ~/laravel-craftsman
 fi
 cp -r builds/ ~/laravel-craftsman/
-mv ~/laravel-craftsman/laravel-craftsman craftsman
+mv ~/laravel-craftsman/laravel-craftsman ~/laravel-craftsman/craftsman
+
+printf "\n"
+success "Deployed Successfully" " SUCCESS "
